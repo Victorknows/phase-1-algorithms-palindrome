ad36f31795c0405193ab135c9af2a78c00136fff
Victor Muhoro <victormuhoro7@gmail.com>
2024-01-14 22:10:56 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    const wordToArray = word.split('');
+    const reverse = wordToArray.slice().reverse();
+    const rejoin = reverse.join('');
+  
+    return rejoin === word;
+  }
+  
 
 /* 
   Add your pseudocode here
+  1.Converts the word to an array of characters
+  2.Reverse the array
+  3.Join the reversed array to a string
+  4.Compare the reversed string with the original word
 */
 
 /*
   Add written explanation of your solution here
+  The function takes in a word and converts it into an array of characters. This array is then reversed
+  and ten turned into a string. The function then compares the reversed string with the original word.
 */
 
 // You can run `node index.js` to view these console logs
